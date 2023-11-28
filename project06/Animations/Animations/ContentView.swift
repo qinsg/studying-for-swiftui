ec2ad5962c2314e0f35f7e0b698be4da35d4b53d
James Qin <7723906@qq.com>
2023-11-28 14:06:13 +0800

    animation

--- project06/Animations/Animations/ContentView.swift
+++ project06/Animations/Animations/ContentView.swift
@@ -8,27 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
     @State private var antimationAmount = 1.0
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        print(antimationAmount)
-        return VStack {
-            Button("Tap Me"){
-                // antimationAmount += 1
-                withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                    antimationAmount += 1
-                }
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) {num in
+                    Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(num) / 2), value: dragAmount)
             }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-//            .rotation3DEffect(
-//                .degrees(antimationAmount),
-//                   axis: (x: 1.0, y: 1.0, z: 0.0)
-//            )
-            .scaleEffect(antimationAmount)
         }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in
+                        withAnimation(.bouncy) {
+                            dragAmount = .zero
+                        }
+                    }
+            )
+            //.animation(.bouncy, value: dragAmount)
+            
+//        print(antimationAmount)
+//        return VStack {
+//            Button("Tap Me"){
+//                enabled.toggle()
+//                // antimationAmount += 1
+////                withAnimation(.spring(duration: 1, bounce: 0.5)) {
+////                    antimationAmount += 1
+////                }
+//            }
+//            .padding(50)
+//            .background(enabled ? .red : .blue)
+//            .foregroundStyle(.white)
+//            .animation(.default, value: enabled)
+////            .rotation3DEffect(
+////                .degrees(antimationAmount),
+////                   axis: (x: 1.0, y: 1.0, z: 0.0)
+////            )
+//            .scaleEffect(antimationAmount)
+//            .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
+//            .animation(.spring(duration: 2, bounce: 0.9), value: enabled)
+//        }
     }
 }
 
