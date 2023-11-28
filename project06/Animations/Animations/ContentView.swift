056595e1935713558221330dd70498671664cd16
James Qin <7723906@qq.com>
2023-11-28 15:20:37 +0800

    day033

--- project06/Animations/Animations/ContentView.swift
+++ project06/Animations/Animations/ContentView.swift
@@ -7,44 +7,82 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     let letters = Array("Hello SwiftUI")
     @State private var antimationAmount = 1.0
     @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     
+    @State private var isShowingRed = false
+    
     var body: some View {
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) {num in
-                    Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount)
-                    .animation(.linear.delay(Double(num) / 2), value: dragAmount)
+        VStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            if isShowingRed{
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.pivot)
+                    //.transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-                .onChanged { dragAmount = $0.translation }
-                .onEnded { _ in
-                    dragAmount = .zero
-                    enabled.toggle()
-                }
-        )
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in
-                        withAnimation(.bouncy) {
-                            dragAmount = .zero
-                        }
-                    }
-            )
+        .onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
+            }
+        }
+//        HStack(spacing: 0) {
+//            ForEach(0..<letters.count, id: \.self) {num in
+//                    Text(String(letters[num]))
+//                    .padding(5)
+//                    .font(.title)
+//                    .background(enabled ? .blue : .red)
+//                    .offset(dragAmount)
+//                    .animation(.linear.delay(Double(num) / 2), value: dragAmount)
+//            }
+//        }
+//        .gesture(
+//            DragGesture()
+//                .onChanged { dragAmount = $0.translation }
+//                .onEnded { _ in
+//                    dragAmount = .zero
+//                    enabled.toggle()
+//                }
+//        )
+//        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+//            .frame(width: 300, height: 200)
+//            .clipShape(.rect(cornerRadius: 10))
+//            .offset(dragAmount)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { dragAmount = $0.translation }
+//                    .onEnded { _ in
+//                        withAnimation(.bouncy) {
+//                            dragAmount = .zero
+//                        }
+//                    }
+//            )
             //.animation(.bouncy, value: dragAmount)
             
 //        print(antimationAmount)
