283d5e39c3373ac3ad00b5d3317d308e53a96a42
James Qin <7723906@qq.com>
2023-12-14 16:51:54 +0800

    day037

---
+++ project07/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by James Qin on 2023/12/12.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
