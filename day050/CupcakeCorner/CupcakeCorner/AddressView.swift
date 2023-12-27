f36589e0951e7d58a4315f34b23d901b993ccd0e
James Qin <7723906@qq.com>
2023-12-27 14:38:03 +0800

    day050

---
+++ day050/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by James Qin on 2023/12/27.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView(order: Order())
+}
---
+++ day050/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,176 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by James Qin on 2023/12/26.
+//
+
+import SwiftUI
+import CoreHaptics
+
+struct SimpleHapticsView: View {
+    @State private var counter = 0
+    
+    
+   var body: some View {
+       Button("Tap count: \(counter)"){
+           counter += 1
+       }
+       //.sensoryFeedback(.increase, trigger: counter)
+       //.sensoryFeedback(.impact(weight: .heavy, intensity: 10), trigger: counter)
+       .sensoryFeedback(.impact(flexibility: .soft, intensity: 0.5), trigger: counter)
+    }
+}
+
+struct CoreHapticsSimpleView: View {
+    @State private var engine: CHHapticEngine?
+    
+    var body: some View {
+        Button("Tap me", action: complexSuccess)
+            .onAppear(perform: {
+                prepareHaptics()
+            })
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        }catch{
+            print("Failed to play patteern: \(error.localizedDescription)")
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+}
+
+struct CoreHapticsComplexView: View {
+    @State private var engine: CHHapticEngine?
+    
+    var body: some View {
+        Button("Tap me", action: complexSuccess)
+            .onAppear(perform: {
+                prepareHaptics()
+            })
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        for i in stride(from: 0, to: 1, by: 0.1){
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+        for i in stride(from: 0, to: 1, by: 0.1){
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        }catch{
+            print("Failed to play patteern: \(error.localizedDescription)")
+        }
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+}
+
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    var name = "Taylor"
+}
+
+struct CodingKeyView: View {
+    var body: some View {
+        Button("Encode Taylor", action: encodeTaylor)
+    }
+    
+    func encodeTaylor() {
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
+    }
+}
+
+struct ContentView:View {
+    @State private var order = Order()
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self){
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Delivery details") {
+                        AddressView(order: order)
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
