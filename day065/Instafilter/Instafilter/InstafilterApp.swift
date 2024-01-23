7ef4689de349b2bbf6bda22e6fbef27512723d57
James Qin <7723906@qq.com>
2024-01-23 14:43:22 +0800

    day065

---
+++ day065/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by James Qin on 2024/1/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
