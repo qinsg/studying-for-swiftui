68100bd904295a48e10a8ad96c22c3193742795d
James Qin <7723906@qq.com>
2024-04-25 14:32:21 +0800

    t

---
+++ day088/Flashzilla/Flashzilla/CardView.swift
@@ -0,0 +1,62 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by James Qin on 2024/4/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    
+    var removal: (() -> Void)? = nil
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .shadow(radius: 10)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundStyle(.secondary)
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(offset.width / 5.0))
+        .offset(x: offset.width * 5)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture(
+            DragGesture()
+                .onChanged{ gesture in
+                    offset = gesture.translation
+                }
+                .onEnded{ _ in
+                    if abs(offset.width) > 100 {
+                        // remove the card
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+    }
+}
+
+#Preview {
+    CardView(card: .example)
+}
