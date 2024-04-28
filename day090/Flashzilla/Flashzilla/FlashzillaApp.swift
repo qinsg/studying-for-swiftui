4669245309bd4f9990dd3a9d2689228fb8930c1f
James Qin <7723906@qq.com>
2024-04-28 11:44:09 +0800

    day090

---
+++ day090/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by James Qin on 2024/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
