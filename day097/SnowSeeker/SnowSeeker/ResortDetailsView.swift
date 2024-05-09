73810a3188d040be5b3592ec2fbab0c1ae6d89cd
James Qin <7723906@qq.com>
2024-05-09 11:02:27 +0800

    day097

---
+++ day097/SnowSeeker/SnowSeeker/ResortDetailsView.swift
@@ -0,0 +1,44 @@
+//
+//  ResortDetailsView.swift
+//  SnowSeeker
+//
+//  Created by James Qin on 2024/5/9.
+//
+
+import SwiftUI
+
+struct ResortDetailsView: View {
+    let resort: Resort
+    var size: String {
+        switch resort.size {
+            case 1: "Small"
+            case 2: "Average"
+            default: "Large"
+        }
+    }
+    var price: String {
+        String(repeating: "$", count: resort.price)
+    }
+    
+    var body: some View {
+        Group {
+            VStack {
+                Text("Size")
+                    .font(.caption.bold())
+                Text(size)
+                    .font(.title3)
+            }
+            VStack {
+                Text("Price")
+                    .font(.caption.bold())
+                Text(price)
+                    .font(.title3)
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+}
+
+#Preview {
+    ResortDetailsView(resort: .example)
+}
